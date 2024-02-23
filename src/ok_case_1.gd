6b15b0585f11fdd47f35a7105d83fe4e4af49900
Butch Wesley <butch.wesley@gmail.com>
2024-02-23 12:33:11 -0500

    comments

--- src/ok_case_1.gd
+++ src/ok_case_1.gd
@@ -1,7 +1,8 @@
 extends SceneTree
 
-# Illustrate that there are no errors if you make reference
-# to Foo before this.
+# Illustrate that there are no errors if you make reference to Foo before
+# calling create_script_that_extends_foo_and_instance as mentioned in
+# error_case_1.gd
 func _init():
 	var f = Foo.new()
 	Dynamic.create_script_that_extends_foo_and_instance()
--- src/ok_case_4.gd
+++ src/ok_case_4.gd
@@ -1,5 +1,7 @@
 extends SceneTree
 
+# Creating one GDScript with a resource_path that is not empty seems to stop
+# the error in future GDScripts with empty resource paths.
 func _init():
 	var DynamicScript = GDScript.new()
 	DynamicScript.source_code = "extends 'res://src/node_uses_foo.gd'"
@@ -12,6 +14,15 @@ func _init():
 	DynamicScript.reload()
 
 	var inst = DynamicScript.new() # OK
+	print(inst)
+	print(inst.f)
+
+	DynamicScript = GDScript.new()
+	DynamicScript.source_code = "extends 'res://src/node_uses_foo.gd'"
+	DynamicScript.resource_path = ''
+	DynamicScript.reload()
+
+	inst = DynamicScript.new() # OK
 	print(inst)
 	print(inst.f)
 
